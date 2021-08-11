e49713fbe7047a1ae94cede24ebe8ca03c61c838
kedareshubham <shubham@sawolabs.com>
2021-08-12 04:34:55 +0530

    init

--- sawoDjango/asgi.py
+++ sawoDjango/asgi.py
@@ -1,16 +1,16 @@
 """
-ASGI config for sawoDjango project.
+ASGI config for SawoDjango project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sawoDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SawoDjango.settings')
 
 application = get_asgi_application()
--- sawoDjango/urls.py
+++ sawoDjango/urls.py
@@ -1,7 +1,7 @@
-"""sawoDjango URL Configuration
+"""SawoDjango URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,11 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.views.generic import RedirectView
+from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("main",include('main.urls')),
-    path('', RedirectView.as_view(url='/main/')),
+    path('',include('app.urls')),
+    path('admin/', admin.site.urls)
+    
 ]
