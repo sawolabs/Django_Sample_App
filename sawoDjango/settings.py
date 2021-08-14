9d1e527b6c54439700511cbe6dbf9098b24241c2
voidniloy <niloy@sawolabs.com>
2021-08-14 15:47:52 +0530

    dotenv setup

--- sawoDjango/settings.py
+++ sawoDjango/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dotenv
+
 # import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
